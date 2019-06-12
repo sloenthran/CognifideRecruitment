4a164d8d66ffb0941d4f7a5f3eaa3812465421e7
Dawid Nogacz <sloenthran@gmail.com>
2019-06-12 19:28:44 +0200

    Create new branch

---
+++ src/main/java/pl/nogacz/cognifide/CognifideApplication.java
@@ -0,0 +1,11 @@
+package pl.nogacz.cognifide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognifideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CognifideApplication.class, args);
+    }
+}
---
+++ src/main/java/pl/nogacz/cognifide/controller/BookDetailsController.java
@@ -0,0 +1,7 @@
+package pl.nogacz.cognifide.controller;
+
+/**
+ * @author Dawid Nogacz on 12.06.2019
+ */
+public class BookDetailsController {
+}
