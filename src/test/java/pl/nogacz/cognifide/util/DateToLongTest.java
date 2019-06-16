205899dda766f24bcd4c47e8b9698f1099dff2c2
Dawid Nogacz <sloenthran@gmail.com>
2019-06-16 17:04:10 +0200

    Add RatingDTO sort test

--- src/test/java/pl/nogacz/cognifide/util/DateToLongTest.java
+++ src/test/java/pl/nogacz/cognifide/util/DateToLongTest.java
@@ -2,12 +2,6 @@ package pl.nogacz.cognifide.util;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.*;
 
 /**
  * @author Dawid Nogacz on 16.06.2019
